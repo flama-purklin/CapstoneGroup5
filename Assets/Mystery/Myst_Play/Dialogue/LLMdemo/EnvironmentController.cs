69715f1bf3b74fc15e0c6fbdb025803ae794d5ff
Charles Belanger <charlesbelanger00@gmail.com>
2025-02-11 17:33:18 -0700

    3B Parameter LM Baby!
    This frees up some vram, which can be used for better language model performance

---
+++ Assets/Mystery/Myst_Play/Dialogue/LLMdemo/EnvironmentController.cs
@@ -0,0 +1,21 @@
+using UnityEngine;
+
+public class EnvironmentController : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        //incoming
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //incoming
+    }
+
+    private void Awake()
+    {
+        Application.targetFrameRate = 30;
+    }
+}
