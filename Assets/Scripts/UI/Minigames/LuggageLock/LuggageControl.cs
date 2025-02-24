e5e94d676747aa55912778bf7d3e3c1de899a68e
Noah Lovelace <ndlovelace13@gmail.com>
2025-02-23 19:48:10 -0700

    Functional Luggage Minigame

---
+++ Assets/Scripts/UI/Minigames/LuggageLock/LuggageControl.cs
@@ -0,0 +1,106 @@
+using System.Collections;
+using UnityEngine;
+
+public class LuggageControl : MonoBehaviour
+{
+    [SerializeField] Animator anim;
+
+    string currentCombo;
+    bool solved = false;
+
+    bool shutdown = false;
+
+    [Header("Number Controls")]
+    [SerializeField] NumberManager firstNum;
+    [SerializeField] NumberManager secondNum;
+    [SerializeField] NumberManager thirdNum;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if ((GameControl.GameController.currentState == GameState.FINAL && !shutdown) || 
+            (Input.GetKeyDown(KeyCode.Escape) && GameControl.GameController.currentState == GameState.MINIGAME))
+        {
+            shutdown = true;
+            ExitLuggage();
+        }
+    }
+
+    public void SetCombo(string newCombo)
+    {
+        //for debugging
+        Debug.Log("New Combination: " + newCombo);
+        solved = false;
+        currentCombo = newCombo;
+        GameControl.GameController.currentState = GameState.MINIGAME;
+        anim.Play("luggageInit");
+    }
+
+    public void ExitLuggage()
+    {
+        StartCoroutine(CloseLuggage());
+    }
+
+    IEnumerator CloseLuggage()
+    {
+        anim.Rebind();
+        anim.Update(0f);
+        anim.Play("luggageCancel");
+
+
+        while (anim.GetCurrentAnimatorStateInfo(0).normalizedTime < 1)
+        {
+            yield return null;
+        }
+
+        //reset game state
+        if (GameControl.GameController.currentState != GameState.FINAL)
+        {
+            GameControl.GameController.currentState = GameState.DEFAULT;
+        }
+
+        gameObject.SetActive(false);
+    }
+
+    public void CheckCombo()
+    {
+        string input = firstNum.currentIndex.ToString() + secondNum.currentIndex.ToString() + thirdNum.currentIndex.ToString();
+        Debug.Log("Testing combination: " + input);
+        if (!solved && input.Equals(currentCombo))
+        {
+            solved = true;
+            StartCoroutine(Unlock());
+        }
+    }
+
+    IEnumerator Unlock()
+    {
+        Debug.Log("Correct Combination!");
+        anim.Play("luggageSuccess");
+
+        yield return new WaitForSeconds(5f);
+
+        anim.Rebind();
+        anim.Update(0f);
+        anim.Play("successExit");
+        while (anim.GetCurrentAnimatorStateInfo(0).normalizedTime < 1)
+        {
+            yield return null;
+        }
+
+        if (GameControl.GameController.currentState != GameState.FINAL)
+        {
+            GameControl.GameController.currentState = GameState.DEFAULT;
+        }
+
+        gameObject.SetActive(false);
+
+
+    }
+}
