ab891dab712873d6ba688d2e19836e965ba390ed
User <user@example.com>
2025-03-25 08:03:54 -0700

    schizo

--- Assets/_Core/Tools/TriggerRecompile.cs
+++
@@ -1,9 +0,0 @@
-using UnityEngine;
-
-/// <summary>
-/// Empty script to trigger recompilation.
-/// </summary>
-public class TriggerRecompile : MonoBehaviour
-{
-    // This script does nothing - just forces Unity to recompile all scripts
-}
