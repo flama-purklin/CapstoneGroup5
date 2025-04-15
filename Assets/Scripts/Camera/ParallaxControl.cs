9d9f943e591b5c56b28fe6c37ce01cd9ef33c6a5
Noah Lovelace <ndlovelace13@gmail.com>
2025-04-14 17:45:43 -0700

    NPC and Player Collision Exception, Parallax Reversing and Pause Fix

--- Assets/Scripts/Camera/ParallaxControl.cs
+++ Assets/Scripts/Camera/ParallaxControl.cs
@@ -20,26 +20,26 @@ public class ParallaxControl : MonoBehaviour
     }
 
     // Update is called once per frame
-    void FixedUpdate()
+    void Update()
     {
         float temp = (timer * (1 - parallaxEffect) * speedMod);
         float dist = (timer * parallaxEffect * speedMod);
 
         transform.localPosition = new Vector3(startPos + dist, transform.localPosition.y, transform.localPosition.z);
 
-        timer += Time.fixedDeltaTime;
+        timer -= Time.unscaledDeltaTime;
 
         
 
         //resets the location once it gets too far from its origin
-        if (dist > startPos + length)
+        if (dist < -(startPos + length))
         {
             
             //startPos -= length;
 
             //this offset should make the teleportation more seamless - might need to be messed with more
-            dist -= (startPos + length);
-            timer = dist / (parallaxEffect * speedMod);
+            dist += (startPos + length);
+            timer = -dist / (parallaxEffect * speedMod);
         }
          
     }
