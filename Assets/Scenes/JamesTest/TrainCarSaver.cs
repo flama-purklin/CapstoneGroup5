5fb0cda1969f2d2a3f56394a2686da2afa4d4c04
Noah Lovelace <ndlovelace13@gmail.com>
2025-04-15 17:26:21 -0700

    Actual Profiles maybe

--- Assets/Scenes/JamesTest/TrainCarSaver.cs
+++ Assets/Scenes/JamesTest/TrainCarSaver.cs
@@ -1,7 +1,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 using System.IO;
-using static UnityEditor.Experimental.GraphView.GraphView;
+//using static UnityEditor.Experimental.GraphView.GraphView;
 using UnityEngine.UI;
 using TMPro;
 
