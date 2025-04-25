41ee32b147c57f12d48caac1fe8aa7090c8ffb2f
User <user@example.com>
2025-04-25 03:27:10 -0700

    Revert "New Dialogue Anims and Evidence Selection Dropdown"
    This reverts commit 4fd78eabebed5e35cd1b2b4f28591873c52e95d6.

--- Assets/Scripts/UI/Dialogue/DialogueUIController.cs
+++ Assets/Scripts/UI/Dialogue/DialogueUIController.cs
@@ -110,7 +110,6 @@ public class DialogueUIController : MonoBehaviour
         
         Debug.Log("[DialogueUIController] About to call canvasFade.FadeIn()");
         canvasFade.FadeIn();
-        
         StartCoroutine(FocusInputNextFrame());
     }
 
@@ -120,7 +119,7 @@ public class DialogueUIController : MonoBehaviour
     public void HideDialogue()
     {
         Debug.Log("[DialogueUIController] HideDialogue called");
-        //canvasFade.FadeOut();
+        canvasFade.FadeOut();
     }
 
     /// <summary>
